8c927dc59ba0f30f12557e4e96dde90f19824a2e
Masnoon Junaid <masnoon20@gmail.com>
2019-06-25 20:10:17 +0530

    required component creation and react route

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Cart extends React.Component{
+  render(){
+    return(
+      <div>
+      <h3>  Hello from the Cart</h3>
+      </div>
+    )
+  }
+}
+export default Cart
---
+++ src/components/Details.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Details extends React.Component {
+  render (){
+    return (
+      <div>
+        <h3>Hello from details</h3>
+      </div>
+    )
+  }
+}
+export default Details
