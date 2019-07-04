0be9891555c555ddc89ec9c7185af0ca89691146
Masnoon Junaid <masnoon20@gmail.com>
2019-07-04 17:09:44 +0530

    button to remove item from cart

--- src/context.js
+++ src/context.js
@@ -76,9 +76,33 @@ class ProductProvider extends Component {
       console.log('this is decrement method');
     }
 
+    removeItem = id => {
+      let tempProducts = [...this.state.products];
+      let tempCart = [...this.state.cart];
+
+      tempCart = tempCart.filter(item => item.id !== id);
+      const index = tempProducts.indexOf(this.getItem(id));
+      let removedProduct = tempProducts[index];
+      removedProduct.inCart = false;
+      removedProduct.count = 0;
+      removedProduct.total = 0;
+
+      this.setState(
+        () => {
+          return {
+            cart: [...tempCart],
+            products: [...tempProducts]
+          };
+        },
+        () => {
+          this.addTotals();
+        }
+      )
+    };
+
     clearCart = () => {
       this.setState(() => {
-        return { cart : []};
+        return { cart : [] };
       }, ()=>{
         this.setProducts();
         this.addTotals();
