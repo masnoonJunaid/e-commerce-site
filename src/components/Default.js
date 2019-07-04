d7c6fdb51c8690807df60b97616b8debaac53599
Masnoon Junaid <masnoon20@gmail.com>
2019-07-04 22:08:36 +0530

    default page 404 not found

--- src/components/Default.js
+++ src/components/Default.js
@@ -4,7 +4,21 @@ class Default extends React.Component {
   render(){
     return (
       <div>
-        <h3>Page not found</h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto text-center text-title text-upperCase pt-5">
+              <h1 className="display-3">404</h1>
+              <h1>error</h1>
+              <h2>page not found</h2>
+              <h3>the requested URL
+                <span className="text-danger">
+                  {this.props.location.pathname}
+                </span>{" "}
+                was not found
+              </h3>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
